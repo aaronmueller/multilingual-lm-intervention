8566d6b408e1d6e670df4c30c67fa8c8988e1e10
YuXia-SR <61883895+YuXia-SR@users.noreply.github.com>
2021-12-29 00:24:17 -0800

    update interventions with multiple language

--- syneval.py
+++ syneval.py
@@ -13,8 +13,8 @@ import statistics
 import vocab_utils as vocab
 
 from utils_num_agreement import batch, convert_results_to_pd
-from experiment_num_agreement1 import Model
-from neuron_experiment_multiple_templates_num_agreement import construct_templates
+from experiment_num_agreement import Model
+from neuron_experiment_multiple_templates_num_agreement import construct_interventions_bi, construct_interventions_fr, construct_templates
 from transformers import (
     GPT2LMHeadModel, GPT2Tokenizer,
     TransfoXLTokenizer,
@@ -28,6 +28,11 @@ from transformers_modified.modeling_xlnet import XLNetLMHeadModel
 from attention_intervention_model import (
     AttentionOverride, TXLAttentionOverride, XLNetAttentionOverride, BertAttentionOverride
 )
+from vocab_utils import get_nouns, get_nouns2, get_verbs, get_verbs2, get_prepositions, \
+        get_preposition_nouns, get_adv1s, get_adv2s
+from generate_all_sentences import load_nouns, load_verbs, load_nouns2, load_verbs2, load_bigrams, \
+        load_adv1, load_adv2, load_prepositions, load_preposition_nouns
+import vocab_utils as vocab
 
 PADDING_TEXT = """In 1991, the remains of Russian Tsar Nicholas II and his family
 (except for Alexei and Maria) are discovered.
@@ -110,6 +115,63 @@ class Intervention():
         self.candidates_tok = [self.enc.convert_tokens_to_ids(tokens)
                                for tokens in self.candidates]
 
+def construct_templates_fr(language):
+    LANG_COMPLEMENTIZERS = {
+        'fr': 'que',
+        'nl': 'die',
+        # German inflects the complementizer for case and number
+        'de': {'M': 'den', 'N': 'das', 'F': 'die'},
+        # Finnish inflects the complementizer for case and number
+        'fi': {'P': 'jota', 'E': 'josta', 'I': 'johon',
+               'Al': 'jolle', 'Ac': 'jonka'}
+    }
+
+    LANG_CONJUNCTIONS = {
+        'fr': 'et',
+        'nl': 'en',
+        'de': 'und',
+        'fi': 'ja'
+    }
+
+    templates = []
+    if attractor in ['prep_singular', 'prep_plural']:
+        for p in load_prepositions(language):
+            for ppns, ppnp in load_preposition_nouns(language):
+                ppn = ppns if attractor == 'prep_singular' else ppnp
+                if language in ("fr", "nl"):
+                    template = ' '.join(['{}', '{}', p, ppn])
+                elif language == "fi":
+                    # Finnish has postpositions w/ genitive nouns
+                    template = ' '.join(['{}', ppn, p])
+                templates.append(template)
+    elif attractor in ['rc_singular', 'rc_plural']:
+        for noun2s, noun2p in load_nouns2(language):
+            noun2 = noun2s if attractor.startswith('rc_singular') else noun2p
+            for verb2s, verb2p in load_verbs2(language):
+                if language == "fi":
+                    case, verb2s = verb2s.split("_")
+                verb2 = verb2s if attractor.startswith('rc_singular') else verb2p
+                if language in ("fr", "nl"):
+                    template = ' '.join(['{}', '{}', LANG_COMPLEMENTIZERS[language], noun2, verb2])
+                elif language == "fi":
+                    template = ' '.join(['{}', LANG_COMPLEMENTIZERS['fi'][case], noun2, verb2])
+                else:
+                    raise ValueError("Invalid language.")
+                templates.append(template)
+    elif attractor == "distractor":
+        if language == "fr":
+            raise ValueError("Cannot place adverbs before a verb in French.")
+        for adv1 in load_adv1(language):
+            for adv2 in load_adv2(language):
+                templates.append(' '.join(['{}', '{}', adv1, LANG_CONJUNCTIONS[language], adv2]))
+    elif attractor == "distractor_1":
+        if language == "fr":
+            raise ValueError("Cannot place adverbs before a verb in French.")
+        for adv1 in load_adv1(language):
+            templates.append(' '.join(['{}', '{}', adv1]))
+    else:
+        templates = ["{} {}"] if language in ("fr", "nl") else ["{}"]
+    return templates
 
 def construct_templates():
     # specify format of inputs. fill in with terminals later
@@ -150,6 +212,81 @@ def construct_templates():
         templates = ['The {}']
     return templates
 
+def construct_interventions_bi(tokenizer, DEVICE, seed, shuffle=False):
+    interventions = {}
+    all_word_count = 0
+    used_word_count = 0
+    temp = "{}"
+    word1_list, word2_list = load_bigrams()
+    if shuffle:
+        random.shuffle(word1_list)
+        random.shuffle(word2_list)
+    for (idx, (word1, word2)) in enumerate(zip(word1_list, word2_list)):
+        if idx + 1 >= len(word1_list):
+            break
+        all_word_count += 1
+        try:
+            intervention_name = '_'.join([temp, word1, word2])
+            interventions[intervention_name] = Intervention(
+                tokenizer,
+                temp,
+                [word1, word1_list[idx+1]],
+                [word2, word2_list[idx+1]],
+                device=DEVICE)
+            used_word_count += 1
+        except AssertionError as e:
+            pass
+    '''
+    print(f"\t Only used {used_word_count}/{all_word_count} nouns due to tokenizer")
+    if examples > 0 and len(interventions) >= examples:     # randomly sample input sentences
+        random.seed(seed)
+        interventions = {k: v 
+                for k, v in random.sample(interventions.items(), examples)}
+    '''
+    interventions = [v for k, v in interventions.items()]
+    return interventions
+
+def construct_interventions_fr(tokenizer, DEVICE, attractor, seed, language):
+    interventions = {}
+    all_word_count = 0
+    used_word_count = 0
+    templates = construct_templates_fr(language)
+    for temp in templates:
+        if attractor.startswith('within_rc'):
+            pass
+        else:
+            for ns, np in load_nouns(language):
+                for v_singular, v_plural in load_verbs(language):
+                    all_word_count += 1
+                    try:
+                        intervention_name = '_'.join([temp, ns, v_singular])
+                        if language == "fi":
+                            interventions[intervention_name] = Intervention(
+                                tokenizer,
+                                temp,
+                                [ns, np],
+                                [v_singular, v_plural],
+                                device=DEVICE)
+                            used_word_count += 1
+                        else:
+                            interventions[intervention_name] = Intervention(
+                                tokenizer,
+                                temp.format(ns.capitalize().split()[0], "{}"),
+                                [ns.split()[1], np.split()[1]],
+                                [v_singular, v_plural],
+                                device=DEVICE)
+                            used_word_count += 1
+                    except Exception as e:
+                        pass
+    '''
+    print(f"\t Only used {used_word_count}/{all_word_count} nouns due to tokenizer")
+    if examples > 0 and len(interventions) >= examples:     # randomly sample input sentences
+        random.seed(seed)
+        interventions = {k: v 
+                for k, v in random.sample(interventions.items(), examples)}
+    '''
+    interventions = [v for k, v in interventions.items()]
+    return interventions
 
 def construct_interventions(tokenizer, DEVICE, attractor, seed):
     interventions = {}
@@ -184,7 +321,7 @@ def construct_interventions(tokenizer, DEVICE, attractor, seed):
                         used_word_count += 1
                     except AssertionError as e:
                         pass
-    # print(f"\t Only used {used_word_count}/{all_word_count} nouns due to tokenizer")
+    #print(f"\t Only used {used_word_count}/{all_word_count} nouns due to tokenizer")
     #if examples > 0 and len(interventions) >= examples:     # randomly sample input sentences
     #    random.seed(seed)
     interventions = [v for k, v in interventions.items()]
@@ -205,7 +342,13 @@ def syneval(model_type, device, random_weights, attractor, seed):
                  BertTokenizer if model.is_bert else
                  XLMRobertaTokenizer).from_pretrained(model_type)
     # Set up folder if it does not exist
-    interventions = construct_interventions(tokenizer, device, attractor, seed)
+    if language != "en":
+        interventions = construct_interventions_fr(tokenizer, device, attractor, seed, language)
+    elif attractor.startswith("bigram"):
+        interventions = construct_interventions_bi(tokenizer, device, seed, shuffle=attractor.endswith("shuffle"))
+    else:
+        interventions = construct_interventions(tokenizer, device, attractor, seed)
+    #interventions = construct_interventions(tokenizer, device, attractor, seed)
     n_correct_singular = 0
     n_correct_plural = 0
     n_multitoken_increase = 0
