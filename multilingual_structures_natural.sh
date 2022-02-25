327fe853424326816176885359ba34ca44bfbb6b
aaronmueller <ammaaronmueller@gmail.com>
2022-02-25 16:40:47 -0500

    multilingual model experiment script

---
+++ multilingual_structures_natural.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/opt/NVIDIA/cuda-10/lib64
+export CUDA_VISIBLE_DEVICES=`free-gpu`
+source /home/amueller/miniconda3/bin/activate
+conda activate xtreme
+
+EN_MODEL="bert-base-multilingual-cased"
+FR_MODEL="bert-base-multilingual-cased"
+DE_MODEL="bert-base-multilingual-cased"
+FI_MODEL="bert-base-multilingual-cased"
+NL_MODEL="bert-base-multilingual-cased"
+
+#python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL none natural language:en && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FI_MODEL none natural language:fi && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $NL_MODEL none natural language:nl && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FR_MODEL none natural language:fr && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $DE_MODEL none natural language:de && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL prep_singular natural language:en && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FI_MODEL prep_singular natural language:fi && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $NL_MODEL prep_singular natural language:nl && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FR_MODEL prep_singular natural language:fr && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $DE_MODEL prep_singular natural language:de && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL prep_plural natural language:en && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FI_MODEL prep_plural natural language:fi && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $NL_MODEL prep_plural natural language:nl && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FR_MODEL prep_plural natural language:fr && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $DE_MODEL prep_plural natural language:de && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL rc_singular natural language:en && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FI_MODEL rc_singular natural language:fi && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FR_MODEL rc_singular natural language:fr && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $NL_MODEL rc_singular natural language:nl && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $DE_MODEL rc_singular natural language:de && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL rc_plural natural language:en && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FI_MODEL rc_plural natural language:fi && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $FR_MODEL rc_plural natural language:fr && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $NL_MODEL rc_plural natural language:nl && wait
+#python neuron_experiment_multiple_templates_num_agreement.py $DE_MODEL rc_plural natural language:de && wait
+python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL distractor natural language:en && wait
+python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL distractor_1 natural language:en && wait
+python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL bigram natural language:en && wait
+python neuron_experiment_multiple_templates_num_agreement.py $EN_MODEL bigram_shuffle natural language:en && wait
